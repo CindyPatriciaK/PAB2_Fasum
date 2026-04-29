e6dcaa000170a71c33b33cad4ac2d3317abd11ba
abii <cindypatriciakadir_2327240008@mhs.mdp.ac.id>
2026-04-29 20:05:19 +0700

    signin

--- flutter_application_fasum/lib/screens/home_screen.dart
+++ flutter_application_fasum/lib/screens/home_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: const Center(child: Text('Welcome to the Home Screen!')),
+    );
   }
 }
